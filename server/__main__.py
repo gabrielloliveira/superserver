9f14cf583bc0eaa3a7e105dc8e4d699cd8c075e4
lucasbezerra26 <lucasbezerra527@gmail.com>
2022-05-07 18:46:56 -0300

    feat: added argparse

--- server/__main__.py
+++ server/__main__.py
@@ -1,8 +1,19 @@
 from random import randint
 
 from .server import Server
-
+from ast import literal_eval
+import argparse
 
 if __name__ == "__main__":
-    server = Server(num_threads=randint(1, 4))
+
+    parser = argparse.ArgumentParser(description="Parâmetros necessários.")
+    parser.add_argument("threads", type=int, metavar="t", help="Número de threads para o servidor.")
+    parser.add_argument("subprocess", metavar="s", type=str, help="É um subprocesso ?")
+
+    args = parser.parse_args()
+    print(args)
+    print(args.threads)
+    print()
+
+    server = Server(num_threads=args.threads, is_subprocess=literal_eval(args.subprocess))
     server.run()
