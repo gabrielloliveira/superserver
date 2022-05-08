a1fe3201272becdb4ca606313cce7338f48634a0
gabrielloliveira <gabrielloliveira097@gmail.com>
2022-05-08 10:59:37 -0300

    fix: argparse arguments

--- server/__main__.py
+++ server/__main__.py
@@ -5,9 +5,24 @@ from .server import Server
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Parâmetros necessários.")
-    parser.add_argument("--threads", type=int, action="store", help="Número de threads para o servidor.", default=1, required=True)
-    parser.add_argument("--subproccess", action="store", help="É um subprocesso ?", required=False, default=False)
+    parser.add_argument(
+        "-t",
+        "--threads",
+        type=int,
+        action="store",
+        help="Número de threads para o servidor.",
+        default=1,
+        required=True,
+    )
+    parser.add_argument(
+        "-p",
+        "--partner",
+        help="Inidica se o servidor é parceiro.",
+        action="store_true",
+        required=False,
+        default=False,
+    )
     args = parser.parse_args()
 
-    server = Server(num_threads=args.threads, is_subprocess=args.subprocess)
+    server = Server(num_threads=args.threads, is_subprocess=args.partner)
     server.run()
